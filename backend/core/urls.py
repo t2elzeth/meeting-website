b2967ac093fcbf19fa5af17599f36860d2d9daeb
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-02-25 22:31:22 +0600

    Update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken')),
 
     path('api/v1/', include('forms.urls')),
