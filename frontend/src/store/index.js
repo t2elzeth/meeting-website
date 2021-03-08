910896800d29e46efbed74d3dbe89e71391fea51
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-03-08 16:41:09 +0600

    Update store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,9 @@
 import {createStore} from 'vuex'
-import questionnaires from "@/store/modules/questionnaires";
 import auth from "@/store/modules/auth";
 
 
 export default createStore({
   modules: {
-    auth, questionnaires
+    auth
   },
 })
